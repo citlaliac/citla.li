a70a2d96356598f84426f703bd2e38cdb3437d95
Citlali <citlaliac@gmail.com>
2025-04-04 15:52:53 -0400

    scroll on tech and tour pages good, safe clone spot

--- projects/javascript/src/pages/tech/ResumePage.js
+++ projects/javascript/src/pages/tech/ResumePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header';
+import '../../styles/ResumePage.css';
 
 function ResumePage() {
   const navigate = useNavigate();
