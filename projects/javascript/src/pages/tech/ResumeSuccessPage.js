ed378fed4b96c3cf19629f3b0943d24219d8ba4a
Citlali <citlaliac@gmail.com>
2025-04-05 14:27:51 -0400

    all working, maybe not server

--- projects/javascript/src/pages/tech/ResumeSuccessPage.js
+++ projects/javascript/src/pages/tech/ResumeSuccessPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header';
-import '../../styles/tech/ResumePDFPage.css';
+import '../../styles/tech/ResumePdfPage.css';
 
 function ResumeSuccessPage() {
   return (
