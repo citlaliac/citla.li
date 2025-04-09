21984724e156530ad259148ea9aa5c68e50caad3
Citlali <citlaliac@gmail.com>
2025-04-08 20:35:16 -0400

    working, on web, tracking across pages, tiem in et

--- projects/javascript/src/pages/SeePage.js
+++ projects/javascript/src/pages/SeePage.js
@@ -21,12 +21,12 @@ const SeePage = () => {
     {
       title: 'spring 2023',
       path: '/photos/spring-2023',
-      image: '/assets/photos/spring-2023/DSC_0311.jpg'
+      image: '/assets/photos/spring-2023/DSC_0353.jpg'
     },
     {
       title: 'spring 2024',
       path: '/photos/spring-2024',
-      image: '/assets/photos/spring-2024/DSC_0387.jpg'
+      image: '/assets/photos/spring-2024/DSC_0387+2.jpg'
     },
     {
       title: 'portrait',
