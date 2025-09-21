c5888055e95022af9cdf3dce35b424a3e4fbe9e0
unknown <citlaliac@gmail.com>
2025-09-21 19:12:36 -0400

    Test deployment - added comment

--- projects/javascript/src/pages/tech/ResumeSuccessPage.js
+++ projects/javascript/src/pages/tech/ResumeSuccessPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header';
-import '../../styles/tech/ResumePdfPage.css';
+import '../../styles/tech/ResumePDFPage.css';
 
 function ResumeSuccessPage() {
   return (
