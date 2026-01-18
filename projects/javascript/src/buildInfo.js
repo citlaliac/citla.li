3f9aa4b6b95664584128cdf2c105bb56ec6c2796
unknown <citlaliac@gmail.com>
2026-01-18 17:34:49 -0500

    Fix remaining Dependabot vulnerabilities in server and projects packages - Updated nodemon from 2.0.22 to 3.1.11 in server package - Added npm overrides for qs and brace-expansion in server package - Fixed form-data and axios vulnerabilities in projects package - All vulnerabilities now resolved (0 in all locations)

--- projects/javascript/src/buildInfo.js
+++ projects/javascript/src/buildInfo.js
@@ -1,4 +1,4 @@
 // This file is auto-generated during build
 // It contains the build/deployment timestamp
-// Generated at: 2026-01-18T22:25:47.237Z
-export const BUILD_DATE = '2026-01-18T22:25:47.237Z';
+// Generated at: 2026-01-18T22:29:09.077Z
+export const BUILD_DATE = '2026-01-18T22:29:09.077Z';
